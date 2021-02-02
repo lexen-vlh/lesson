be692826801a7102912419da27509c356caf7e8f
lexen-vlh / <filaa@volhovez.natm.ru>
2021-02-02 14:09:11 +0300

    03_02

---
+++ lesson03_02.py
@@ -0,0 +1,4 @@
+def user(name, surname, year_of_birth, city, email, phone):
+    print(name, surname, year_of_birth, city, email, phone)
+
+user(name='User', surname='User', year_of_birth=1999, city='Nov', email='user@mail', phone='79990000000')
